5e7d5f420585f8d8e19d5b071d94efe18590a699
Oleksii Kolesnyk <olexii.kolesnyk.w@gmail.com>
2023-04-19 17:31:25 +0300

    Added simple reply bot

---
+++ TGHub.WebApiCore/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Telegram.Bot;
+using TGHub.WebApiCore.Options;
+
+namespace TGHub.WebApiCore;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddTelegramBotClient(this IServiceCollection services)
+    {
+        services.AddTransient<ITelegramBotClient, TelegramBotClient>(provider =>
+        {
+            var settings = provider.GetRequiredService<IOptionsSnapshot<TelegramBotOptions>>();
+            return new TelegramBotClient(settings.Value.Token);
+        });
+
+        return services;
+    }
+}
