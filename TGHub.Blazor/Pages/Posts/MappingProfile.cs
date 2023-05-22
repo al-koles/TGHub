3567c61593353b10ed460101f2e4197bf6feb5b9
Oleksii Kolesnyk <olexii.kolesnyk.w@gmail.com>
2023-05-22 15:31:33 +0300

    Added calendar events statuses

---
+++ TGHub.Blazor/Pages/Posts/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using TGHub.Application.Common.Mappings;
+using TGHub.Application.Services.Posts;
+using TGHub.Blazor.Shared.Components.Calendar.Models;
+using TGHub.Domain.Entities;
+
+namespace TGHub.Blazor.Pages.Posts;
+
+public class MappingProfile : IMapWith<object>
+{
+    public void Mapping(Profile profile)
+    {
+        profile.CreateMap<PostSendStatus, CalendarEventStatus>().ConvertUsing((srs, _) => srs switch
+        {
+            PostSendStatus.Sent => CalendarEventStatus.Succeed,
+            PostSendStatus.FailedToSend => CalendarEventStatus.Error,
+            PostSendStatus.Scheduled => CalendarEventStatus.Pending,
+            PostSendStatus.NotScheduled => CalendarEventStatus.Warning,
+            _ => throw new ArgumentOutOfRangeException(nameof(srs), srs, null)
+        });
+        profile.CreateMap<Lottery, CalendarEventModel>().ReverseMap();
+        profile.CreateMap<Post, CalendarEventModel>().ReverseMap();
+    }
+}
--- TGHub.Blazor/Shared/Components/Calendar/Models/CalendarEventModel.cs
+++ TGHub.Blazor/Shared/Components/Calendar/Models/CalendarEventModel.cs
@@ -1,20 +1,10 @@
-﻿using AutoMapper;
-using TGHub.Application.Common.Mappings;
-using TGHub.Domain.Entities;
+﻿namespace TGHub.Blazor.Shared.Components.Calendar.Models;
 
-namespace TGHub.Blazor.Shared.Components.Calendar.Models;
-
-public class CalendarEventModel : IMapWith<Lottery>
+public class CalendarEventModel
 {
     public int Id { get; set; }
     public long? TelegramId { get; set; }
     public string Title { get; set; } = null!;
     public DateTime ReleaseDateTime { get; set; }
     public CalendarEventStatus Status { get; set; }
-
-    public void Mapping(Profile profile)
-    {
-        profile.CreateMap<Lottery, CalendarEventModel>().ReverseMap();
-        profile.CreateMap<Post, CalendarEventModel>().ReverseMap();
-    }
 }
