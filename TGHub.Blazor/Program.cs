1ac3ad27e4be45bffc938f280c6d7c5f29da5157
Oleksii Kolesnyk <olexii.kolesnyk.w@gmail.com>
2023-05-26 20:54:06 +0300

    Fixed lottery scheduling

--- TGHub.Blazor/Program.cs
+++ TGHub.Blazor/Program.cs
@@ -48,9 +48,8 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseHttpsRedirection();
 }
-
-// app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
@@ -66,5 +65,6 @@ app.MapControllers();
 
 await app.MigrateDbContextIfNecessaryAsync<TgHubDbContext>();
 await app.SchedulePostsAsync();
+await app.ScheduleLotteriesAsync();
 
 app.Run();
