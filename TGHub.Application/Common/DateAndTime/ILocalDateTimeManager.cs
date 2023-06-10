4199add1859f3bb210fb8c8e0eec440818d3f418
Oleksii Kolesnyk <olexii.kolesnyk.w@gmail.com>
2023-06-10 19:51:42 +0300

    Added LocalDateTimeManager

---
+++ TGHub.Application/Common/DateAndTime/ILocalDateTimeManager.cs
@@ -0,0 +1,18 @@
+﻿namespace TGHub.Application.Common.DateAndTime;
+
+public enum To
+{
+    Utc = -1,
+    Local = 1
+}
+
+public interface ILocalDateTimeManager
+{
+    event Action? OnOffsetChanged;
+    event Func<Task>? OnOffsetChangedAsync;
+    TimeSpan Offset { get; set; }
+    void SetOffsetByTimeZoneId(string timeZoneId);
+    Task SetFromJsAsync();
+    (DateOnly, TimeOnly) ConvertDateAndTime(DateOnly date, TimeOnly time, To format);
+    DateTime ConvertDateTime(DateTime dateTime, To format);
+}
---
+++ TGHub.Application/Common/DateAndTime/LocalDateTimeManager.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.JSInterop;
+
+namespace TGHub.Application.Common.DateAndTime;
+
+public class LocalDateTimeManager : ILocalDateTimeManager
+{
+    private readonly IJSRuntime _jsRuntime;
+    private TimeSpan _offset;
+
+    public LocalDateTimeManager(IJSRuntime jsRuntime)
+    {
+        _jsRuntime = jsRuntime;
+    }
+
+    public TimeSpan Offset
+    {
+        get => _offset;
+        set
+        {
+            _offset = value;
+            OnOffsetChanged?.Invoke();
+            OnOffsetChangedAsync?.Invoke();
+        }
+    }
+
+    public event Action? OnOffsetChanged;
+    public event Func<Task>? OnOffsetChangedAsync;
+
+    public void SetOffsetByTimeZoneId(string timeZoneId)
+    {
+        var timeZone = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
+        var offset = timeZone.GetUtcOffset(DateTime.UtcNow);
+
+        Offset = TimeSpan.FromHours((int)offset.TotalHours);
+    }
+
+    public async Task SetFromJsAsync()
+    {
+        var offset = await _jsRuntime.InvokeAsync<int>("getClientTimeZoneOffset");
+        Offset = TimeSpan.FromHours(offset);
+    }
+
+    public (DateOnly, TimeOnly) ConvertDateAndTime(DateOnly date, TimeOnly time, To format)
+    {
+        var resultDateTime = date.ToDateTime(time) + Offset * (int)format;
+
+        date = DateOnly.FromDateTime(resultDateTime);
+        time = TimeOnly.FromDateTime(resultDateTime);
+
+        return (date, time);
+    }
+
+    public DateTime ConvertDateTime(DateTime dateTime, To format)
+    {
+        return dateTime + (int)format * Offset;
+    }
+}
